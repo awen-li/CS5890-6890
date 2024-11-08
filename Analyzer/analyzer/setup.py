3ceb039b6ce07db9a6af8e7f581af22c626a7963
Awen <48666726+awen-li@users.noreply.github.com>
2024-11-08 10:25:40 -0700

    Update setup.py

--- Analyzer/analyzer/setup.py
+++ Analyzer/analyzer/setup.py
@@ -2,12 +2,12 @@ import os
 from distutils.core import setup, Extension
 
 
-cgdump_moudle = Extension('analyzer',
+mymoudle = Extension('analyzer',
                     define_macros = [('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
                     extra_link_args=[],
                     extra_compile_args=['-std=c++11'],
                     include_dirs = ['./include'],
-                    libraries = ['shmQueue', "comgraph"],
+                    libraries = ['shmQueue'],
                     library_dirs = [],
                     sources = ['./source/analyzer.cpp', './source/ext.cpp'])
 
@@ -16,6 +16,6 @@ setup(
     name='myAnalyzer',
     version='1.0',
     description='A Python package with a C extension for reading from shared memory',
-    ext_modules=[cgdump_moudle]
+    ext_modules=[mymoudle]
 )
 
