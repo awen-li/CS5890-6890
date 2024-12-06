373ab5901685c74aea6a76fa5bd597e18378a9a4
awen-li <li.wen@wsu.edu>
2024-12-06 10:06:08 -0800

    add task2 for gdb

---
+++ practices/gdb-pyc/task2/task.py
@@ -0,0 +1,5 @@
+import taskc
+
+stuId = 1
+stuName = taskc.query(stuId)
+print (stuName)
---
+++ practices/gdb-pyc/task2/taskc/taskc.c
@@ -0,0 +1,60 @@
+#include <Python.h>
+
+typedef struct _student_
+{
+    unsigned stuId;
+    char stuName [32];
+}STU;
+
+static inline void getStuName (unsigned stuID, char** stuName)
+{
+    STU allStudents[] = {{1, "alice"}, {2, "bob"}, {3, "jack"}, {5, "ami"}};
+
+    for (unsigned i = 0; i < sizeof(allStudents)/sizeof(STU); i++)
+    {
+        if (stuID == allStudents[i].stuId)
+        {
+            *stuName = allStudents[i].stuName;
+            return;
+        }
+    }
+
+    *stuName = "None";
+    return;
+}
+
+static PyObject* query(PyObject *self, PyObject *args) 
+{
+    int stuId = 0;
+    
+    if (!PyArg_ParseTuple(args, "i", &stuId)) 
+    {
+        return NULL;
+    }
+    
+    char* stuName = NULL;
+    getStuName (stuId, &stuName);
+    
+    return PyUnicode_FromString(stuName);
+}
+
+static PyMethodDef taskMethods[] = 
+{
+    {"query", query, METH_VARARGS, "query student name"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef taskmodule = 
+{
+    PyModuleDef_HEAD_INIT,
+    "taskc", /* name of module */
+    NULL,    /* module documentation */
+    -1,      /* size of per-interpreter state */
+    taskMethods
+};
+
+PyMODINIT_FUNC PyInit_taskc(void) 
+{
+    return PyModule_Create(&taskmodule);
+}
+
